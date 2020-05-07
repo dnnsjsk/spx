235514514cdf3aea1517daaea9c1c03005ecbe56
Dennis <dnnsjsk@gmail.com>
2020-05-07 16:18:46 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         filename: 'spx.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'js')
     },
     plugins: [
         new CopyPlugin(
